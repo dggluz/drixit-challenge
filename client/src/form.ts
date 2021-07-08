185d78758f0997e20a9db0560fae9677533a946d
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2021-07-07 22:01:31 -0300

    [client][refactor] Moving JS code to TS

---
+++ client/src/form.ts
@@ -0,0 +1,18 @@
+const getById = (id: string) => {
+    const ret = document.getElementById(id);
+
+    if (ret === null) {
+        throw new Error(`There is no element with "${id}" id`);
+    }
+
+    return ret;
+}
+
+const emailForm = getById('email-form');
+const passwordForm = getById('password-form');
+
+emailForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    passwordForm.style.display = '';
+});
