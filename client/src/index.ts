6e5f2ee5a0bf8fbebef326c91a88147086e9bade
Gonzalo Gluzman <gonzalo.gluzman@stensul.com>
2021-07-10 14:36:49 -0300

    [client] WIP: making user info page

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,11 @@
-import './components/form';
+import { renderUserInfo } from './pages/user-info/user-info.page';
+
+renderUserInfo($('#app'), {
+	id: "it-drixit-1",
+	avatar: "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+	email: "it@drixit.com",
+	name: "IT",
+	surname: "Drixit",
+	age: 25,
+	role: "admin"
+});
---
+++ client/src/pages/user-info/user-info.page.ts
@@ -0,0 +1,46 @@
+import { ClientUser } from '../../model/model';
+
+const assert = <T> (typeGuard: (x: unknown) => x is T, expectedType?: string) => (x: unknown) => {
+    if (!typeGuard(x)) {
+        const errorMessage = expectedType ?
+            `Unexpected type "${typeof x}", ${expectedType} expected`:
+            `Unexpected type "${typeof x}"`
+        ;
+        throw new Error(errorMessage);
+    }
+    return x;
+};
+
+const isStr = (x: unknown): x is string =>
+    typeof x === 'string'
+;
+
+const str = assert(isStr);
+
+const html = str(require('./user-info.page.html').default);
+
+const $get = ($wrapper: JQuery<HTMLElement>, selector: string) => {
+    const $ret = $wrapper.find(selector);
+
+    if ($ret.length !== 1) {
+        throw new Error(`Couldn't find element matching the selector "${selector}"`);
+    }
+
+    return $ret;
+};
+
+export const renderUserInfo = ($wrapper: JQuery<HTMLElement>, userInfo: ClientUser) => {
+    const $dom = $(html);
+
+    $get($dom, '.name').text(`${userInfo.name} ${userInfo.surname}`);
+    $get($dom, '.id').text(userInfo.id);
+    $get($dom, '.avatar').attr('src', userInfo.avatar);
+    $get($dom, '.age').text(userInfo.age);
+    $get($dom, '.email').text(userInfo.email);
+    $get($dom, '.role').text(userInfo.role === 'admin' ? 'Admin' : 'Regular user');
+
+    $wrapper
+        .empty()
+        .append($dom)
+    ;
+};
